7036aae2b597f9dd6b017e4f5c640ca0a63aa9c4
yndgow <yndgow@gmail.com>
2023-01-13 02:15:05 +0900

    security

--- Ch08/src/main/java/kr/co/ch08/service/User2Service.java
+++ Ch08/src/main/java/kr/co/ch08/service/User2Service.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import kr.co.ch08.repository.User2Repo;
@@ -22,16 +23,20 @@ public class User2Service implements UserDetailsService{ // 로그인 수행 인
 		
 	}
 	public void selectUser2List() {}
-//	public User2VO selectUser2One(User1VO vo) {
-//		return repo.findUser2VOByUidAndPass(vo.getUid(), vo.getPass());
-//	}
+	public User2VO selectUser2One(User1VO vo) {
+		return repo.findUser2VOByUidAndPass(vo.getUid(), vo.getPass());
+	}
 	public void updateUser2() {}
 	public void deleteUser2() {}
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	// 인증처리
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User2VO user = repo.findById(username).get();
+		
 		if(user == null) {
 			System.out.println("user empty...");
 			throw new UsernameNotFoundException(username);
@@ -39,9 +44,10 @@ public class User2Service implements UserDetailsService{ // 로그인 수행 인
 		
 		UserDetails userDts = User.builder()
 									.username(user.getUid())
-									.password(user.getPass())
+									.password(passwordEncoder.encode(user.getPass()))
 									.roles("ADMIN")
 									.build();
+		System.out.println(passwordEncoder.encode(user.getPass()));
 		return userDts;
 	}
 }
