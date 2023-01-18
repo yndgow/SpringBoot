cc2eed329733475b134884a4b03651523ba8eea6
yndgow <yndgow@gmail.com>
2023-01-19 01:40:47 +0900

    2023/01/19 fetch

--- sBoard/src/main/resources/static/js/register.js
+++ sBoard/src/main/resources/static/js/register.js
@@ -1,4 +1,4 @@
-$(()=>{
+/*$(()=>{
 	$('#checkUid').click(()=>{
 		
 		let uid = $('input[name=uid]').val()
@@ -17,4 +17,28 @@ $(()=>{
 			
 		})
 	})
-})
+})*/
+document.addEventListener("DOMContentLoaded", ()=>{
+	
+	let btnCheckUid = document.getElementById('checkUid');
+	
+	// 아이디 중복 체크
+	btnCheckUid.addEventListener('click', (e)=>{
+		let uid = document.querySelector('input[name=uid]')
+		url = '/SBoard/user/checkUid/'+uid.value;
+		fetch(url)
+		.then(res => {
+			if(!res.ok) throw new Error(res.statusText);
+			return res.json();
+			})
+		.then(data => {
+			if(data.result > 0){
+				alert('이미 사용중인 아이디입니다.')
+			}else{
+				alert('사용가능한 아이디입니다.')
+			}
+			})
+		.catch(err => console.log(err)) ;
+	})
+	
+});
