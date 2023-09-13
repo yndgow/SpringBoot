3180c2fa66b5afa219684ea6439e54f95cd2e27f
yndgow <yndgow@gmail.com>
2023-09-13 17:50:06 +0900

    2023.09.13 h2, sql, repository

--- bank/src/test/java/com/tencoding/bank/BankApplicationTests.java
+++ bank/src/test/java/com/tencoding/bank/BankApplicationTests.java
@@ -3,11 +3,11 @@ package com.tencoding.bank;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class BankApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
